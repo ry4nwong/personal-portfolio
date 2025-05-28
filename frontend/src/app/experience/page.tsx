f69697382d75a4c932e78aee5474bcb4a08bf107
Ryan Wong <r3wong@ucsd.edu>
2025-05-28 12:50:30 -0700

    Experience JSON and removed API call

--- frontend/src/app/experience/page.tsx
+++ frontend/src/app/experience/page.tsx
@@ -2,30 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { BriefcaseBusiness, ArrowUpRight } from 'lucide-react';
 import FormattedDate from "../components/FormattedDate";
-import { formatDate } from "../utils/formatDate";
-
-interface Experience {
-    title: string
-    company: string
-    slug: string
-    icon: string
-    start_date: string
-    end_date: string
-    header: string
-}
+import experience from '@/data/experience/experience.json'
 
 export default async function ExperiencePage() {
-    const res = await fetch('http://localhost:8000/api/experience/', {
-        cache: 'no-store',
-    });
-
-    if (!res.ok) {
-        return <div className="text-center text-red-500 mt-100">Failed to load experience</div>
-    }
-
-    const experiences: Experience[] = await res.json();
-    experiences.sort((a, b) => new Date(b.start_date).getTime() - new Date(a.start_date).getTime());
-
     return (
         <main className="mx-auto">
             <div className="flex flex-col px-10 py-10 gap-5 border border-[var(--border)] bg-[var(--card-bg-dark)] rounded-2xl w-full mb-8">
@@ -42,11 +21,11 @@ export default async function ExperiencePage() {
             </div>
 
             <ol className="relative border-s-2 border-[var(--muted)] lg:ms-30 ms-20">
-                {experiences.map((experience, idx) => (
+                {experience.map((experience, idx) => (
                     <li key={idx} className="relative mb-15 ms-6 py-5">
                         <span className="absolute -start-8 top-20 flex items-center justify-center w-3 h-3 rounded-full ring-4 ring-white bg-[var(--primary)]" />
                         <time className="absolute -start-35 top-16 text-md text-[var(--foreground)] w-16 text-right">
-                            {formatDate(experience.start_date)}
+                            {experience.start_date}
                         </time>
                         <div className="ml-10 px-10 py-10 max-w-2xl border border-[var(--border)] rounded-2xl bg-[var(--card-bg)] ">
                             <Image
@@ -69,8 +48,6 @@ export default async function ExperiencePage() {
                                 View More
                                 <ArrowUpRight size={15} />
                             </Link>
-
-
                         </div>
                     </li>
                 ))}
