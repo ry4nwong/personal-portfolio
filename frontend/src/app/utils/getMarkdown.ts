c68173cf5abc1e9d9a1c00985c78ce39a6401645
Ryan Wong <r3wong@ucsd.edu>
2025-06-02 14:51:02 -0700

    server path edit

--- frontend/src/app/utils/getMarkdown.ts
+++ frontend/src/app/utils/getMarkdown.ts
@@ -1,11 +1,11 @@
 import { remark } from 'remark';
 import html from 'remark-html';
 import fs from 'fs';
-import path from 'path';
+import serverPath from './serverPath';
 
 export async function getMarkdownData(filePath: string): Promise<string> {
-  const res = await fetch(filePath);
-  const fileContents = await res.text();
+  const fullPath = serverPath(filePath);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   const processedContent = await remark()
     .use(html)
