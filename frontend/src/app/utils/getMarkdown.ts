f21c97f071fc6f87f7ade1fd92e2fe2f9797e32d
Ryan Wong <r3wong@ucsd.edu>
2025-06-03 15:13:16 -0700

    updated filepath for fetch

--- frontend/src/app/utils/getMarkdown.ts
+++ frontend/src/app/utils/getMarkdown.ts
@@ -1,19 +1,24 @@
 import { remark } from 'remark';
 import html from 'remark-html';
-import fs from 'fs';
-import path from 'path';
+import { headers } from 'next/headers';
 
 export async function getMarkdownData(filePath: string): Promise<string> {
   // const fullPath = path.join(process.cwd(), filePath);
   // const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const h = await headers();
+  const host = h.get('host');
+  const protocol = host?.startsWith('localhost') ? 'http' : 'https';
+  const baseUrl = `${protocol}://${host}`;
+
   const res = await fetch(`${baseUrl}${filePath}`);
   const fileContents = await res.text();
 
   const processedContent = await remark()
     .use(html)
     .process(fileContents);
+
+  console.log(baseUrl);
 
   return processedContent.toString();
 }
