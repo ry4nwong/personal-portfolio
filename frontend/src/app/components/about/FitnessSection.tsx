6a7b5202402fffcd0f1079492b343fae43331bdf
Ryan Wong <r3wong@ucsd.edu>
2025-06-03 13:21:26 -0700

    Linting on double quotes

--- frontend/src/app/components/about/FitnessSection.tsx
+++ frontend/src/app/components/about/FitnessSection.tsx
@@ -1,21 +1,21 @@
-import { BicepsFlexed } from "lucide-react";
-import { getMarkdownData } from "@/app/utils/getMarkdown";
+import { BicepsFlexed } from 'lucide-react';
+import { getMarkdownData } from '@/app/utils/getMarkdown';
 
 export default async function FitnessSection() {
-    const description = await getMarkdownData("src/data/about/fitness.md");
+  const description = await getMarkdownData('src/data/about/fitness.md');
 
-    return (
-        <section className="flex flex-col px-10 py-10 gap-5 card bg-[var(--card-bg-dark)]">
-            <div className="mb-3 px-3">
-                <BicepsFlexed size={40} />
-            </div>
-            <h1 className="px-3 text-4xl font-bold text-left text-[var(--foreground)]">
-                Fitness
-            </h1>
-            <article
-                className="prose"
-                dangerouslySetInnerHTML={{ __html: description }}
-            />
-        </section>
-    );
+  return (
+    <section className="flex flex-col px-10 py-10 gap-5 card bg-[var(--card-bg-dark)]">
+      <div className="mb-3 px-3">
+        <BicepsFlexed size={40} />
+      </div>
+      <h1 className="px-3 text-4xl font-bold text-left text-[var(--foreground)]">
+        Fitness
+      </h1>
+      <article
+        className="prose"
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
+    </section>
+  );
 }
--- frontend/src/app/components/about/HistorySection.tsx
+++ frontend/src/app/components/about/HistorySection.tsx
@@ -1,21 +1,21 @@
-import { BookOpen } from "lucide-react";
-import { getMarkdownData } from "@/app/utils/getMarkdown";
+import { BookOpen } from 'lucide-react';
+import { getMarkdownData } from '@/app/utils/getMarkdown';
 
 export default async function HistorySection() {
-    const description = await getMarkdownData("src/data/about/history.md");
+  const description = await getMarkdownData('src/data/about/history.md');
 
-    return (
-        <section className="flex flex-col px-10 py-10 gap-5 card bg-[var(--card-bg-dark)]">
-            <div className="mb-3 px-3">
-                <BookOpen size={40} />
-            </div>
-            <h1 className="px-3 text-4xl font-bold text-left text-[var(--foreground)]">
-                History
-            </h1>
-            <article
-                className="prose"
-                dangerouslySetInnerHTML={{ __html: description }}
-            />
-        </section>
-    );
+  return (
+    <section className="flex flex-col px-10 py-10 gap-5 card bg-[var(--card-bg-dark)]">
+      <div className="mb-3 px-3">
+        <BookOpen size={40} />
+      </div>
+      <h1 className="px-3 text-4xl font-bold text-left text-[var(--foreground)]">
+        History
+      </h1>
+      <article
+        className="prose"
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
+    </section>
+  );
 }
